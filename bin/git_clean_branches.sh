2e1c76049978a364e7607ffad2008459cb30fdfe
James Hogge <jameshogge@gmail.com>
2025-07-03 03:33:37 +0100

    Fix script

--- bin/git_clean_branches.sh
+++ bin/git_clean_branches.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 # Get branches marked as [gone]
-to_delete=$(git branch -vv | awk '/\[gone\]/ {print $1}')
+to_delete=$(git branch -vv | grep 'gone\]' | awk '{print $1}')
 
 # If no branches to delete
 if [[ -z "$to_delete" ]]; then
@@ -10,7 +10,5 @@ if [[ -z "$to_delete" ]]; then
 else
     count=$(echo "$to_delete" | wc -l | tr -d ' ')
     echo "Deleting $count branches..."
-    echo "$to_delete"
-    # Use xargs safely, in case of special characters
     echo "$to_delete" | xargs -n 1 git branch -D
 fi
